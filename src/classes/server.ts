e9cf8657f9fc62240060f732f98547229a787c42
junglans <cderegistro@gmail.com>
2019-06-24 21:06:41 +0200

    Configurando el socket

--- src/classes/server.ts
+++ src/classes/server.ts
@@ -1,19 +1,28 @@
-import express from 'express';
+import { SERVER_PORT } from '../global/environment';
+
 import bodyParser from 'body-parser';
 import  router  from '../routes/router';
-import { SERVER_PORT } from '../global/environment';
 import cors from 'cors';
+import express from 'express';
+import socketIO from 'socket.io';
+import http from 'http';
 
 export default class Server {
 
     public app: express.Application;
     public port: number;
+    public socket: socketIO.Server;
+    private httpServer: http.Server;
 
     public constructor() {
 
         this.app = express();
         this.port = SERVER_PORT;
+        // Configuramos el socket.
+        this.httpServer = new http.Server(this.app);
+        this.socket = socketIO(this.httpServer);
 
+        this.listenOnSocket();
     }
 
     public start(callback: Function) {
@@ -26,5 +35,12 @@ export default class Server {
 
         this.app.use('/', router);
         this.app.listen( this.port, callback() );
+    }
+
+    private listenOnSocket() {
+        console.log('Escuchando en el socket...');
+        this.socket.on('connection', client => {
+            console.log('Cliente conectado.')
+        });
     }
 }
