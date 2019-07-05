a6e6131e21342493f4828646cc6a11349909518d
junglans <cderegistro@gmail.com>
2019-07-05 14:33:07 +0200

    Se añade un comentario

--- src/global/environment.ts
+++ src/global/environment.ts
@@ -1,2 +1,5 @@
-// Puerto del servidor de express.
+// Puerto del servidor de express. 
+// You should see that the value of PORT is undefined on your computer. 
+// Cloud hosts like Heroku or Azure, however, use the PORT variable to tell 
+// you on which port your server should listen for the routing to work.
 export const SERVER_PORT: number = Number(process.env.PORT) || 3000;
